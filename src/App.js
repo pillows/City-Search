4478ca6d825c146087f148ab24b18bda47c2b25b
Parakram Basnet <parakram.basnet@gmail.com>
2019-01-09 10:54:13 -0500

    Added ZipCode.jsx and InputForm.jsx to handle zip code calls and input forms

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ZipCode from './components/ZipCode';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+    return(
+    <div>
+      <h1 id= "header-layout">Zip and City Search</h1>
+      <ZipCode />
+
+    </div>
     );
   }
 }
---
+++ src/components/ZipCode.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import InputForm from "../components/InputForm.jsx"
+
+class GetCity extends React.Component{
+
+    render(){
+
+        return(<InputForm searchTerm="Queens"/>);
+    }
+}
+
+export default GetCity; 
